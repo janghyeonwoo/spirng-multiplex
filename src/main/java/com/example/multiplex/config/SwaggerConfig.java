bc05fdf156a6a3fd4a738da020722ab6c8ae93ae
SMART-TN-038 <blue672@naver.com>
2022-07-13 17:10:16 +0900

    Swagger 3.0 추가

---
+++ src/main/java/com/example/multiplex/config/SwaggerConfig.java
@@ -0,0 +1,67 @@
+package com.example.multiplex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        List<ResponseMessage> responseMessages = new ArrayList<>();
+        responseMessages.add(new ResponseMessageBuilder()
+                .code(200)
+                .message("OK ~~")
+                .build());
+        responseMessages.add(new ResponseMessageBuilder()
+                .code(404)
+                .message("Not Found ~~")
+                .build());
+        responseMessages.add(new ResponseMessageBuilder()
+                .code(500)
+                .message("Internal Server Error ~~")
+                .build());
+
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(true) //기본응답코드
+                .groupName("게시판")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.multiplex.controller"))
+                .paths(PathSelectors.ant("/board/**"))
+                .build()
+                .apiInfo(apiInfo()).globalResponseMessage(RequestMethod.GET,responseMessages);
+    }
+
+
+    @Bean
+    public Docket api2() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .groupName("S3")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.multiplex.controller"))
+                .paths(PathSelectors.ant("/s3/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Practice Swagger")
+                .description("practice swagger config")
+                .version("1.0")
+                .build();
+    }
+}
