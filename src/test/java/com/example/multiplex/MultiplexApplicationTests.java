bb268878fd4eb721ddd5c6c2c602e891a2b131b1
SMART-TN-038 <blue672@naver.com>
2022-07-13 10:15:45 +0900

    init

---
+++ src/test/java/com/example/multiplex/MultiplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multiplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
