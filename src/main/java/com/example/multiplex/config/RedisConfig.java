63bfbe7e569836233322bcc1b1c169afafaa7861
SMART-TN-038 <blue672@naver.com>
2023-10-04 11:15:37 +0900

    Redis 트랜잭션 추가

--- src/main/java/com/example/multiplex/config/RedisConfig.java
+++ src/main/java/com/example/multiplex/config/RedisConfig.java
@@ -43,6 +43,8 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+        //Transaction
+        redisTemplate.setEnableTransactionSupport(true);
         return redisTemplate;
     }
 
